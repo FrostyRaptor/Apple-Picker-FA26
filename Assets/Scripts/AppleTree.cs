f2708b0abda02595353c6908d4890651df824383
FrostyRaptor <124116207+FrostyRaptor@users.noreply.github.com>
2026-09-15 18:24:27 -0500

    added a bunch of stuff
    -> Only the good stuff

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+  [Header("Inscribed")]
+
+  // Prefab for instantiating apples
+  public GameObject applePrefab;
+
+  // Speed at which the AppleTree moves
+  public float speed = 1f;
+
+  // Distance where AppleTree turns around
+  public float leftAndRightEdge = 10f;
+
+  // Chance that the AppleTree will change directions
+  public float changeDirChance = 0.1f;
+
+  // Seconds between Apples instantiations
+  public float appleDropDelay = 1f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    // Start dropping apples
+    Invoke("DropApple", 2f);
+  }
+
+  void DropApple()
+  {
+    GameObject apple = Instantiate<GameObject>(applePrefab);
+    apple.transform.position = transform.position;
+    Invoke("DropApple", appleDropDelay);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // Basic movement
+    Vector3 pos = transform.position;
+    pos.x += speed * Time.deltaTime;
+    transform.position = pos;
+
+    // Changing Direction
+    if (pos.x < -leftAndRightEdge)
+    {
+      speed = Mathf.Abs(speed); // Move Right
+    }
+    else if (pos.x > leftAndRightEdge)
+    {
+      speed = -Mathf.Abs(speed); // Move Left
+    }
+  }
+
+  void FixedUpdate()
+  {
+    if (Random.value < changeDirChance)
+    {
+      speed *= -1; // Change Direction
+    }
+  }
+}
