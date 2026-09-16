66a1751dc4596cdcbdd0f0110952f7ab0924cd26
FrostyRaptor <124116207+FrostyRaptor@users.noreply.github.com>
2026-09-16 11:23:06 -0500

    added high score stuff

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,16 +10,19 @@ public class ApplePicker : MonoBehaviour
   public int numBaskets = 3;
   public float basketBottomY = -14f;
   public float basketSpacingY = 2f;
+  public List<GameObject> basketList;
 
   // Start is called before the first frame update
   void Start()
   {
+    basketList = new List<GameObject>();
     for (int i = 0; i < numBaskets; i++)
     {
       GameObject tBasketG0 = Instantiate<GameObject>(basketPrefab);
       Vector3 pos = Vector3.zero;
       pos.y = basketBottomY + (basketSpacingY * i);
       tBasketG0.transform.position = pos;
+      basketList.Add(tBasketG0);
     }
   }
 
@@ -28,6 +32,16 @@ public class ApplePicker : MonoBehaviour
     foreach (GameObject tempG0 in appleArr)
     {
       Destroy(tempG0);
+    }
+
+    int basketIndex = basketList.Count - 1;
+    GameObject basketG0 = basketList[basketIndex];
+    basketList.RemoveAt(basketIndex);
+    Destroy(basketG0);
+
+    if (basketList.Count == 0)
+    {
+      SceneManager.LoadScene("_Scene_0");
     }
   }
 }
